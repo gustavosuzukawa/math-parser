41781f8ae1b443e3f594c1b2f00be6b29657ee8b
gustavosuzukawa <16106695+gustavosuzukawa@users.noreply.github.com>
2023-07-07 15:43:11 -0300

    Update ExpressionNode.php

--- src/MathParser/Parsing/Nodes/ExpressionNode.php
+++ src/MathParser/Parsing/Nodes/ExpressionNode.php
@@ -96,6 +96,11 @@ class ExpressionNode extends Node
                 $this->associativity = self::RIGHT_ASSOC;
                 break;
 
+            case '%':
+                $this->precedence = 20;
+                $this->associativity = self::LEFT_ASSOC;
+                break;
+
             default:
                 throw new UnknownOperatorException($operator);
         }
